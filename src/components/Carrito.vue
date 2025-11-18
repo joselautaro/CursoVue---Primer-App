db3f95dccc59e8412c5d6aa3bcafa518f9985703
Lautaro Montaña <joselautarom@gmail.com>
2025-11-18 09:53:37 -0300

    [FEATURE] Carrito

---
+++ src/components/Carrito.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="carrito">
+        <h2>Carrito</h2>
+        <p v-if="estado.items.length === 0">El carrito está vacio</p>
+        <ul v-else>
+            <li v-for="(item, idx) in estado.items" :key="item.id">
+
+                {{ item.nombre }} - ${{ item.precio }} x {{ item.cantidad }}
+                ${{ item.precio * item.cantidad}}
+
+                <button @click="reducirCantidad(idx)">-1</button>
+                <button @click="quitar(idx)">Eliminar</button>     
+            </li>
+        </ul>
+        <div v-if="estado.items.length > 0" class="resumen">
+            <p>Total: {{ total }}</p>
+            <p>Cantidad total de productos: {{ cantidad }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import { useCarrito } from '@/composables/useCarrito';
+
+    export default {
+        name: 'Carrito',
+
+        setup(){
+            const{estado, quitar, reducirCantidad, cantidad, total} = useCarrito();
+
+            return {estado, quitar, reducirCantidad, cantidad, total};
+        }
+    }
+
+</script>
+
+<style scoped>
+    .carrito{
+        padding: 12px;
+        border-radius: 8px;
+        background-color: black;
+    }
+    button{
+        margin-left: 8px;
+    }
+
+    .resumen{
+        margin-top: 12px;
+        font-weight: bold;
+    }
+</style>
+
